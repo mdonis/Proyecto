22db27f5aee596fe0945e862f95437e6f171b3e7
mdonis <78662691+mdonis@users.noreply.github.com>
2021-03-02 18:19:19 -0600

    v1:reading_shuffle_slicing_dataframe_analysis

--- proyecto_mdonis(spyder).py
+++ proyecto_mdonis(spyder).py
@@ -5,3 +5,33 @@ Created on Mon Mar  1 16:59:23 2021
 @author: mdonis
 """
 
+import os
+import numpy as np
+import pandas as pd
+os.chdir('C:/Users/mdonis/Documents/Maestría Data Science/Ciencia de Datos en Python/Proyecto')
+
+ds = np.load('proyecto_training_data.npy')
+
+np.random.shuffle(ds)
+
+n = int(ds.shape[0]*0.8)
+
+train_data = ds[0:n,:]
+test_data = ds[n:,:]
+
+# Mismos sets de datos convertidos en dataframes
+df = pd.DataFrame(ds, columns = ['SalePrice','OverallQual','1stFlrSF','TotRmsAbvGrd','YearBuilt','LotFrontage'])
+train_df = pd.DataFrame(train_data, columns = ['SalePrice','OverallQual','1stFlrSF','TotRmsAbvGrd','YearBuilt','LotFrontage'])
+test_df = pd.DataFrame(test_data, columns = ['SalePrice','OverallQual','1stFlrSF','TotRmsAbvGrd','YearBuilt','LotFrontage'])
+
+# Análisis exploratorio numpy
+mean = train_data.mean(axis = 0)
+maxx = train_data.max(axis = 0)
+minn = train_data.min(axis = 0)
+
+# Análisis exploratorio pandas
+mean_pandas = df.mean(axis = 0)
+max_pandas = df.max(axis = 0)
+min_pandas = df.min(axis = 0)
+std_pandas = df.std(axis = 0)
+rng_pandas = max_pandas - min_pandas
